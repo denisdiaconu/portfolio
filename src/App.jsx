46f787a063562a7aad4c3173aaeb27375e4c498a
denisdiaconu <denisdiaconu117@yahoo.com>
2025-07-23 11:14:55 +0200

    Add framer motion package

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,19 @@ import Projects from './components/Projects';
 
 function App() {
   return (
-    <>
+    <div className='app'>
       {' '}
       <Navbar />
       <Hero />
       <Projects />
       <Contact />
-    </>
+      <>
+        <p>
+          &copy; {new Date().getFullYear()} Denis Diaconu. All rights
+          reserved.
+        </p>
+      </>
+    </div>
   );
 }
 
